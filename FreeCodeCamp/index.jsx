a0bddb1cce1f1b3e2cd76b5e15a2a028f0f959a1
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-20 11:11:33 +0530

    feat: update index page

---
+++ FreeCodeCamp/index.jsx
@@ -0,0 +1,27 @@
+class Examples extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      examples: [
+        {
+          name: "From Props",
+          address: "/fromprops.html",
+        },
+        {
+          name: "Array Filter",
+          address: "/arrayfilter.html",
+        }
+      ]
+    };
+  }
+
+  render() {
+    const examples = this.state.examples.map( (example) =>
+      <a href={example.address}>{example.name}</a>);
+    return examples;
+  }
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Examples />);
