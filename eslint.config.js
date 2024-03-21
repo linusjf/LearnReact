c63be6fd2542c7c47061ea1b0c99af32f5936e3a
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-22 04:50:05 +0530

    fix: fix prettier config for trailing commas

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,47 @@
 const react = require("eslint-plugin-react");
 
-module.exports = [{
-  files: ["**/*.jsx", "**/*.tsx"],
-  languageOptions: {
-    ecmaVersion: "latest",
-    globals: {
-      JSX: true,
-      PropTypes: true,
-      React: true,
-      ReactDOM: true,
-      ReactDOMServer: true,
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
+module.exports = [
+  {
+    files: ["**/*.jsx", "**/*.tsx", "**/*.js"],
+    languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "module",
-    },
-  },
-  linterOptions: {
-    noInlineConfig: false,
-    reportUnusedDisableDirectives: "error",
-  },
-  plugins: {
-    react,
-  },
-  rules: {
-    "comma-dangle": ["warn", "never"],
-    quotes: [
-      "warn",
-      "double",
-      {
-        avoidEscape: true,
+      globals: {
+        JSX: true,
+        PropTypes: true,
+        React: true,
+        ReactDOM: true,
+        ReactDOMServer: true
       },
-    ],
-    "require-jsdoc": "off",
-  },
-  settings: {
-    react: {
-      version: "detect",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
     },
-  },
-}, ];
+    linterOptions: {
+      noInlineConfig: false,
+      reportUnusedDisableDirectives: "error"
+    },
+    plugins: {
+      react
+    },
+    rules: {
+      "comma-dangle": ["warn", "never"],
+      quotes: [
+        "warn",
+        "double",
+        {
+          avoidEscape: true
+        }
+      ],
+      "require-jsdoc": "off"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+];
