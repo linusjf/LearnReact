41b439b13d3506b2618f0f3602eaa94d0c57f9e1
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-22 08:44:30 +0530

    feat: add more freecodecamp jsx snippets

---
+++ FreeCodeCamp/create-a-react-component.jsx
@@ -0,0 +1,24 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // Change code below this line
+return <div><h1>Hello React!</h1></div>;
+
+
+    // Change code above this line
+  }
+};
+
+class Kitten extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+};
---
+++ FreeCodeCamp/create-a-stateless-functional-component.jsx
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+  // Change code below this line
+return <div>Some string of text</div>;
+// Change code above this line
+}
+
+const DemoComponent = function() {
+  return (
+    <div className='customClass' />
+  );
+};
