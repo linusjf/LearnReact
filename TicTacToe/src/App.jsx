76d638d1a0ab8e4245741189a7b9a11c36f29a85
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-23 21:05:38 +0530

    feat: start on history feature for TicTacToe

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -8,14 +8,9 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
-      setSquares(Array(9).fill(null));
-      setXIsNext(true);
       return;
     }
     const nextSquares = squares.slice();
@@ -24,8 +19,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -37,7 +31,6 @@ export default function Board() {
       status = "Next player: " + (xIsNext ? "X" : "O");
     else status = "Game over!";
   }
-
   return (
     <>
       <div className="status">{status}</div>
@@ -57,6 +50,28 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
