b42939e7dc7d8713aa1ccb54203f906c538d74ff
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-23 08:40:53 +0530

    feat: add more freecodecamp jsx snippets

---
+++ FreeCodeCamp/pass-an-array-as-props.jsx
@@ -0,0 +1,20 @@
+const List = (props) => {
+  return <p>{props.tasks.join(',')}</p>
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <List tasks={["walk dog", "workout", "bake cake"]}/>
+        <h2>Tomorrow</h2>
+        <List tasks={["pay electricity bill", "meetup", "zumba class"]}/>
+      </div>
+    );
+  }
+};
---
+++ FreeCodeCamp/write-a-react-component-from-scratch.jsx
@@ -0,0 +1,13 @@
+// Change code below this line
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return <div><h1>My First React Component!</h1></div>;
+  }
+}
+
+const root = document.getElementById("challenge-node");
+ReactDOM.render(<MyComponent/>, root);
