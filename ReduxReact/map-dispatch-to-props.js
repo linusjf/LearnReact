533b517d75d60e8fa66e59ea2ac1936789ba73ac
Linus Fernandes <linus.fernandes@gmail.com>
2024-03-29 10:45:02 +0530

    feat: add freecodecamp redux-react snippets

---
+++ ReduxReact/map-dispatch-to-props.js
@@ -0,0 +1,16 @@
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// Change code below this line
+const mapDispatchToProps = (dispatch) => {
+return {
+submitNewMessage: function(msg) {
+  dispatch(addMessage(msg));
+}
+};
+
+}
