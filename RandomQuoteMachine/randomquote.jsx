d38c000f54c02a6844934748848096e91ae8ace6
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-02 20:49:45 +0530

    fix: format

--- RandomQuoteMachine/randomquote.jsx
+++ RandomQuoteMachine/randomquote.jsx
@@ -28,81 +28,89 @@ class RandomQuote extends React.Component {
   }
 
   handleSubmit = () => {
-   this.getNewQuote();
-  }
+    this.getNewQuote();
+  };
 
   async componentDidMount() {
-     await this.getNewQuote();
+    await this.getNewQuote();
   }
 
   getNewQuote = async () => {
-  const url = "https://public-api.wordpress.com/rest/v1.1/sites/quiteaquote.in/posts/?"+
-    new URLSearchParams({
-      pretty: true,
-      context: "display",
-      page: 1,
-      number: 1,
-      fields: "found",
-      category: "thought for today",
-      type: "post",
-      status: "publish"
-    }).toString();
+    const url =
+      "https://public-api.wordpress.com/rest/v1.1/sites/quiteaquote.in/posts/?" +
+      new URLSearchParams({
+        pretty: true,
+        context: "display",
+        page: 1,
+        number: 1,
+        fields: "found",
+        category: "thought for today",
+        type: "post",
+        status: "publish"
+      }).toString();
 
-  const result = await fetch(url)
-    .then(response => response.json());
-  console.log("Fetched from: " + url);
-  console.log(result);
-  const count = result.found;
-  const rnd = Math.ceil(Math.random() * count);
-  await this.getRandomQuoteIdx(rnd);
-}
-
-getRandomQuoteIdx = async (index) => {
-  const url = "https://public-api.wordpress.com/rest/v1.1/sites/quiteaquote.in/posts/?"+
-    new URLSearchParams({
-      pretty: true,
-      context: "display",
-      page: index,
-      number: 1,
-      fields: "content, title",
-      category: "thought for today",
-      type: "post",
-      status: "publish"
-    }).toString();
-
-  const result = await fetch(url)
-    .then(response => response.json());
-  console.log("Fetched from: " + url);
-  console.log(result);
-  const title = result.posts[0].title;
-  const content = result.posts[0].content;
-  const author = title.split(":", 1)[0];
-  const quote = {
-    content: content,
-    author: author
+    const result = await fetch(url).then((response) => response.json());
+    console.log("Fetched from: " + url);
+    console.log(result);
+    const count = result.found;
+    const rnd = Math.ceil(Math.random() * count);
+    await this.getRandomQuoteIdx(rnd);
   };
-  console.log(quote);
-  this.setState(quote);
-}
+
+  getRandomQuoteIdx = async (index) => {
+    const url =
+      "https://public-api.wordpress.com/rest/v1.1/sites/quiteaquote.in/posts/?" +
+      new URLSearchParams({
+        pretty: true,
+        context: "display",
+        page: index,
+        number: 1,
+        fields: "content, title",
+        category: "thought for today",
+        type: "post",
+        status: "publish"
+      }).toString();
+
+    const result = await fetch(url).then((response) => response.json());
+    console.log("Fetched from: " + url);
+    console.log(result);
+    const title = result.posts[0].title;
+    const content = result.posts[0].content;
+    const author = title.split(":", 1)[0];
+    const quote = {
+      content: content,
+      author: author
+    };
+    console.log(quote);
+    this.setState(quote);
+  };
 
   render() {
     return (
       <section id="quote-box" className="quote-box">
-        <div id="text" className="quote-content"
-        dangerouslySetInnerHTML={
-          {__html: this.state.content}}>
-        </div>
+        <div
+          id="text"
+          className="quote-content"
+          dangerouslySetInnerHTML={{ __html: this.state.content }}
+        ></div>
         <p id="author" className="quote-author">
           {this.state.author}
         </p>
-        <button id="new-quote" className="new-quote-button" type="button" onClick={this.handleSubmit}>
+        <button
+          id="new-quote"
+          className="new-quote-button"
+          type="button"
+          onClick={this.handleSubmit}
+        >
           New Quote
         </button>
         <a
           href="https://twitter.com/intent/tweet"
           id="tweet-quote"
           className="tweet-quote"
-        ><i class="fa-brands fa-x-twitter"></i></a>
+        >
+          <i class="fa-brands fa-x-twitter"></i>
+        </a>
       </section>
     );
   }
