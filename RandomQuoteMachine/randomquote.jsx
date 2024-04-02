c30b9cc67425e387970dda745e017104a2d02b0f
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-02 20:07:20 +0530

    feat: set innerhtml to html content from quiteaquote.in

--- RandomQuoteMachine/randomquote.jsx
+++ RandomQuoteMachine/randomquote.jsx
@@ -27,16 +27,75 @@ class RandomQuote extends React.Component {
     };
   }
 
+  handleSubmit = () => {
+   this.getNewQuote();
+  }
+
+  async componentDidMount() {
+     await this.getNewQuote();
+  }
+
+  getNewQuote = async () => {
+  const url = "https://public-api.wordpress.com/rest/v1.1/sites/quiteaquote.in/posts/?"+
+    new URLSearchParams({
+      pretty: true,
+      context: "display",
+      page: 1,
+      number: 1,
+      fields: "found",
+      category: "thought for today",
+      type: "post",
+      status: "publish"
+    }).toString();
+
+  const result = await fetch(url)
+    .then(response => response.json());
+  console.log("Fetched from: " + url);
+  console.log(result);
+  const count = result.found;
+  const rnd = Math.ceil(Math.random() * count);
+  await this.getRandomQuoteIdx(rnd);
+}
+
+getRandomQuoteIdx = async (index) => {
+  const url = "https://public-api.wordpress.com/rest/v1.1/sites/quiteaquote.in/posts/?"+
+    new URLSearchParams({
+      pretty: true,
+      context: "display",
+      page: index,
+      number: 1,
+      fields: "content, title",
+      category: "thought for today",
+      type: "post",
+      status: "publish"
+    }).toString();
+
+  const result = await fetch(url)
+    .then(response => response.json());
+  console.log("Fetched from: " + url);
+  console.log(result);
+  const title = result.posts[0].title;
+  const content = result.posts[0].content;
+  const author = title.split(":", 1)[0];
+  const quote = {
+    content: content,
+    author: author
+  };
+  console.log(quote);
+  this.setState(quote);
+}
+
   render() {
     return (
       <section id="quote-box" className="quote-box">
-        <div id="text" className="quote-content">
-          {this.state.content}
+        <div id="text" className="quote-content"
+        dangerouslySetInnerHTML={
+          {__html: this.state.content}}>
         </div>
         <p id="author" className="quote-author">
           {this.state.author}
         </p>
-        <button id="new-quote" className="new-quote-button">
+        <button id="new-quote" className="new-quote-button" type="button" onClick={this.handleSubmit}>
           New Quote
         </button>
         <a
