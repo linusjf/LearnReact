afd811e660725fb632af22be78c42a9157053e60
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-08 16:13:03 +0530

    feat: start on markdownpreview.

---
+++ MarkdownPreviewer/markdownpreview.jsx
@@ -0,0 +1,26 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    customizeHtmlSanitizer();
+  }
+
+  render() {
+    return <MarkdownPreview />;
+  }
+}
+
+class MarkdownPreview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+  }
+
+  render() {
+    return <section />;
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
