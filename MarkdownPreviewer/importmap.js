dcadb8557d2dd68f93ce2618c505d58291e11305
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-10 12:56:22 +0530

    feat: add editor box to markdown previewer

--- MarkdownPreviewer/importmap.js
+++ MarkdownPreviewer/importmap.js
@@ -5,7 +5,9 @@ im.textContent = JSON.stringify({
     react: "https://esm.sh/react?dev",
     "react-dom/client": "https://esm.sh/react-dom/client?dev",
     "react-bootstrap": "https://esm.sh/react-bootstrap?dev",
-    "react-markdown": "https://esm.sh/react-markdown@9?bundle"
+    "react-markdown": "https://esm.sh/react-markdown@9?bundle",
+    "react-redux": "https://esm.sh/react-redux?dev",
+    "@reduxjs/toolkit": "https://esm.sh/@reduxjs/toolkit?dev"
   }
 });
 document.currentScript.after(im);
--- MarkdownPreviewer/main.jsx
+++ MarkdownPreviewer/main.jsx
@@ -4,6 +4,17 @@ import Markdown from "react-markdown";
 import { Button } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import { Form } from "react-bootstrap";
+import {
+  configureStore,
+  createSlice
+} from "@reduxjs/toolkit";
+
+const initialState = {
+  input: "",
+  editorMax: false,
+  previewMax: false,
+  status: "idle"
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -43,7 +54,31 @@ class MarkdownEditor extends React.Component {
           <p className="flex-fill text-left mt-2 mb-2">Editor</p>
           <i className="fa fa-arrows-alt"></i>
         </Card.Header>
-        <Form.Control as="textarea" controlId="editor" rows={15} />
+        <Form.Control as="textarea" id="editor" rows={15} />
+      </Card>
+    );
+  }
+}
+
+class HTMLPreview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      html: ""
+    };
+  }
+
+  render() {
+    return (
+      <Card>
+        <Card.Header
+          as="div"
+          className="editor-header d-flex flex-row bg-primary align-items-center"
+        >
+          <p className="flex-fill text-left mt-2 mb-2">Preview</p>
+          <i className="fa fa-arrows-alt"></i>
+        </Card.Header>
+        <Form.Control as="textarea" id="preview" rows={15} />
       </Card>
     );
   }
