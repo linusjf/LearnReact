6322cc3aafaa245d222ca22904e49acf46247926
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-14 19:59:45 +0530

    feat: add drum pads

--- DrumMachine/importmap.js
+++ DrumMachine/importmap.js
@@ -5,7 +5,7 @@ im.textContent = JSON.stringify({
     react: "https://esm.sh/react?dev",
     "react-dom/client": "https://esm.sh/react-dom/client?dev",
     "react-bootstrap": "https://esm.sh/react-bootstrap?dev",
-    "react-audio-player": "https://esm.sh/react-audio-player?dev"
+    "react-h5-audio-player": "https://esm.sh/react-h5-audio-player?dev"
   }
 });
 document.currentScript.after(im);
--- DrumMachine/main.jsx
+++ DrumMachine/main.jsx
@@ -2,7 +2,8 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Form } from "react-bootstrap";
 import { Card } from "react-bootstrap";
-import drumsets from "./drummachine.js";
+import AudioPlayer from "react-h5-audio-player";
+import drumsets, {drumsetnames} from "./drummachine.js";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,21 +11,34 @@ class App extends React.Component {
   }
 
   render() {
-    return(
-    <DrumMachine/>
-    );
+    return <DrumMachine />;
   }
 }
 
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      power: true,
+      volume: 0.5,
+      drumset: 0
+    };
   }
 
   render() {
-    return(
-    <Card id="drum-machine" className="drum-machine">
-</Card>
+    const currDrumSet = drumsets[drumsetnames[this.state.drumset]];
+    return (
+      <Card id="drum-machine" className="drum-machine">
+        <div className="drum-pads-container">
+          {
+            Object.entries(currDrumSet).map(([key, value]) => (
+              <div className="drum-pad">
+            <audio key={key} src={value.url} controls={false} id={value.id} volume={this.state.volume} title={key}>
+            </audio><p>{value.id}</p>
+                </div>))
+            }
+        </div>
+      </Card>
     );
   }
 }
