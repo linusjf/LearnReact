d2d0898d46ca818c583225e911327c41857f7f66
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-18 12:32:39 +0530

    refactor: refactor ThinkingReact

---
+++ ThinkingReact/src/FilterableProductTable.jsx
@@ -0,0 +1,13 @@
+import ProductTable from "./ProductTable";
+import SearchBar from "./SearchBar";
+
+function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
+
+export default FilterableProductTable;
